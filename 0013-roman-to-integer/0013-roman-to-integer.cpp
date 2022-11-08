e9c7c077ca3673a3d246c0934979268675999569
Ahmed Mabrouk Said <105928025+AhmedMabrouk22@users.noreply.github.com>
2022-11-08 16:57:46 +0200

    Time: 43 ms (15.77%), Space: 8.4 MB (18.91%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -3,19 +3,19 @@ public:
 	int romanToInt(string s) {
 		string ch = "IVXLCDM";
 		int arr[7] = { 1, 5, 10, 50, 100, 500, 1000 };
-		map<char, pair<int,int>>mp;
+		unordered_map<char,int>mp;
 		for (int i = 0; i < 7; ++i)
-			mp[ch[i]] = { arr[i], i };
+			mp[ch[i]] = arr[i];
 
 		int res = 0;
 		s += '!';
 		for (int i = 0; i < s.size(); ++i)
 		{
-			if (mp[s[i]].second >= mp[s[i + 1]].second)
-				res += mp[s[i]].first;
+			if (mp[s[i]] >= mp[s[i + 1]])
+				res += mp[s[i]];
 			else
 			{
-				res += mp[s[i + 1]].first - mp[s[i]].first;
+				res += mp[s[i + 1]] - mp[s[i]];
 				i += 1;
 			}
 		}
