e946c8fae3a6b65042ca590310fa2d7aac65e0dc
Ahmed Mabrouk Said <105928025+AhmedMabrouk22@users.noreply.github.com>
2023-02-04 14:02:45 +0200

    Time: 2122 ms (5.02%), Space: 58.2 MB (5.73%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size())
+            return false;
+        
+        sort(s1.begin(),s1.end());
+        for(int i = 0 ; i <= s2.size() - s1.size(); ++i)
+        {
+            string str = s2.substr(i,s1.size());
+            sort(str.begin(),str.end());
+                        
+            if (s1 == str)
+                return true;
+        }
+        return false;
+    }
+};
