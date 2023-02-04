2468e0a2ccd1b3cf81ca63afc01c581d5cbda29c
Ahmed Mabrouk Said <105928025+AhmedMabrouk22@users.noreply.github.com>
2023-02-04 14:18:51 +0200

    Time: 21 ms (35.90%), Space: 7.7 MB (27.24%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
+    map<char,int>mp1,mp2;
     bool checkInclusion(string s1, string s2) {
+        
         if (s1.size() > s2.size())
             return false;
         
-        sort(s1.begin(),s1.end());
-        for(int i = 0 ; i <= s2.size() - s1.size(); ++i)
+        for(auto i : s1)
+            ++mp1[i];
+        
+        for(int i = 0 ; i < s1.size(); ++i)
+            ++mp2[s2[i]];
+        if (valid())
+            return true;
+        
+        int r = s1.size();
+        for(int i = 0 ; i < s2.size() && r < s2.size() ; ++i)
         {
-            string str = s2.substr(i,s1.size());
-            sort(str.begin(),str.end());
-                        
-            if (s1 == str)
+            ++mp2[s2[r]];
+            --mp2[s2[i]];
+            if (valid())
                 return true;
+            ++r;
         }
         return false;
+    }
+    
+    bool valid()
+    {
+        for(char i ='a' ; i <= 'z' ; ++i)
+        {
+            if (mp1[i] != mp2[i])
+                return false;
+        }
+        return true;
+            
     }
 };
