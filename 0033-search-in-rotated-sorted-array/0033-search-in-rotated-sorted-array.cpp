5b4257a75d59677257ad09dc65e25f4d2c93d74a
Ahmed Mabrouk Said <105928025+AhmedMabrouk22@users.noreply.github.com>
2023-03-24 08:39:23 +0200

    Time: 3 ms (81.06%), Space: 11.1 MB (44.39%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,10 +1,29 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        unordered_map<int,int>mp;
-        for(int i = 0 ; i < nums.size() ; ++i)
-            mp[nums[i]] = i;
-        
-        return (mp.count(target) ? mp[target] : -1);
+        int l = 0, r = nums.size() - 1;
+        while(l<=r) 
+        {
+            int mid = (l+r) >> 1;
+            if (nums[mid] == target)
+                return mid;
+            
+            if (nums[l] <= nums[mid]) 
+            {
+                if (target > nums[mid] || target < nums[l])
+                    l = mid + 1;
+                else 
+                    r = mid - 1;
+            }
+            else 
+            {
+                if (target < nums[mid] || target > nums[r])
+                    r = mid - 1;
+                else 
+                    l = mid + 1;
+            }
+
+        }
+        return -1;
     }
 };
