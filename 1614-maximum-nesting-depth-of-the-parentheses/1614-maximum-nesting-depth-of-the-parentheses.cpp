66313f8e593efc8a58891bac8b6e1ac5b7c98694
Ahmed Mabrouk Said <105928025+AhmedMabrouk22@users.noreply.github.com>
2024-04-04 06:13:18 +0200

    Time: 3 ms (40.00%), Space: 7.5 MB (49.50%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        stack<char> st;
         int res = 0;
+        int cnt = 0;
         for (auto i : s)
         {
-
             if (i == '(') 
-                st.push('(');
-
-            if (!st.empty() && i == ')')
-                st.pop();
-
-            res = max(res, (int)st.size());
+                ++cnt;
+            else if (i == ')')
+                --cnt;
+            res = max(res, cnt);
         }
         return res;
     }
