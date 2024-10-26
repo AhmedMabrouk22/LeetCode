a80ee38f393e5c0aa9502f9045af500d97c4d4c9
Ahmed Mabrouk Said <105928025+AhmedMabrouk22@users.noreply.github.com>
2024-10-26 23:35:21 +0300

    Time: 2 ms (88.64%), Space: 48.3 MB (34.40%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,28 +10,51 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        Stack<ListNode> st = new Stack<>();
-        for(ListNode cur = head; cur != null ; cur = cur.next)
-            st.push(cur);
+        
+        if (head.next == null) return;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prv = head;
 
-        int size = st.size();
-        int idx = 0;
-        ListNode nxt = null;
-        ListNode tail;
-        for(ListNode cur = head; cur != null ;) {
-            if (idx == size / 2) {
-                cur.next = null;
-                break;
-            }
-            nxt = cur.next;
-            tail = st.pop();
-            cur.next = tail;
-            tail.next = nxt;
-            cur = nxt;
-            ++idx;
+        // when fast node reach to the end
+        // the slow node will be in the middle
+
+        while (fast != null && fast.next != null) {
+            prv = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        for (ListNode cur = head; cur != null; cur = cur.next)
-            System.out.print(cur.val + " ");
+        prv.next = null;
+
+        // reverse the second half part of list
+
+        ListNode cur = slow;
+        ListNode nxt = null;
+        ListNode prev = null;
+
+        while (cur != null) {
+            nxt = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = nxt;
+        }
+
+        // merge the first part with the second part
+
+        ListNode first = head;
+        ListNode firstNxt = null;
+        ListNode secondNxt = null;
+        while (first != null && prev != null) {
+            firstNxt = first.next;
+            secondNxt = prev.next;
+            first.next = prev;
+            if (firstNxt != null)
+                prev.next = firstNxt;
+
+            first = firstNxt;
+            prev = secondNxt;
+        }
     }
 }
