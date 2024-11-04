cd47fc6cfe5140e2866847c8c5537d6e76d8ff79
Ahmed Mabrouk Said <105928025+AhmedMabrouk22@users.noreply.github.com>
2024-11-04 20:01:02 +0200

    Time: 0 ms (100.00%), Space: 41.4 MB (37.94%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -2,19 +2,7 @@ class Solution {
 
     public boolean rotateString(String s, String goal) {
         if (s.length() != goal.length()) return false;
-        int length = s.length();
-        char[] sChars = s.toCharArray();
-
-        for (int rotationCount = 0; rotationCount < length; ++rotationCount) {
-            sChars = rotateOnce(sChars);
-            if (new String(sChars).equals(goal)) return true;
-        }
-        return false;
-    }
-    private char[] rotateOnce(char[] arr) {
-        char firstChar = arr[0];
-        System.arraycopy(arr, 1, arr, 0, arr.length - 1);
-        arr[arr.length - 1] = firstChar;
-        return arr;
+        String temp = s + s;
+        return temp.contains(goal);
     }
 }
